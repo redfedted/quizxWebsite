9a4bce79ff33a7642e6389d5c00358a771d93193
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-16 00:37:50 +0530

    Comit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,65 @@ import Info from "./components/Info";
 import Interaction from "./components/Interaction";
 import Loop from "./components/Loop";
 import Navbar from "./components/Navbar";
+import "./index.css";
 
 function App() {
+  const items1 = [
+    "Knowledge-Centric",
+    "Connect the Dots",
+    "Cross-Field Collaboration",
+    "LLM-Powered Tools",
+    "Modern-insights to Academics / Cutting-Edge to Academics",
+    "Contextual Learning",
+    "Knowledge-Centric",
+    "Connect the Dots",
+    "Cross-Field Collaboration",
+    "LLM-Powered Tools",
+    "Modern-insights to Academics / Cutting-Edge to Academics",
+    "Contextual Learning",
+  ];
+  const items2 = [
+    "Simple yet Interesting",
+    "Learning Through Play",
+    "Inspiring Ideas",
+    "Curious by Design",
+    "Selectively Gamified",
+    "Collective Intelligence",
+    "Beyond Syllabus",
+    "Simple yet Interesting",
+    "Learning Through Play",
+    "Inspiring Ideas",
+    "Curious by Design",
+    "Selectively Gamified",
+    "Collective Intelligence",
+    "Beyond Syllabus",
+  ];
+  const items3 = [
+    "Social Brainstorming",
+    "Beyond Syllabus",
+    "Creative Classrooms",
+    "Imagining Possibilities",
+    "Fun Meets Functionality",
+    "Student-Machine Interface",
+    "Lecture Synthesizer",
+    "Social Brainstorming",
+    "Beyond Syllabus",
+    "Creative Classrooms",
+    "Imagining Possibilities",
+    "Fun Meets Functionality",
+    "Student-Machine Interface",
+    "Lecture Synthesizer",
+  ];
+
   return (
     <div>
       <Navbar />
       <Info />
-      <Loop />
+      <section className="flex flex-col gap-[0.5rem] bg-[#cf0804] py-[1.125rem] mb-[1.472rem]">
+        <Loop baseVelocity={1} items={items1}></Loop>
+        <Loop baseVelocity={-1} items={items2}></Loop>
+        <Loop baseVelocity={1} items={items3}></Loop>
+      </section>
       <Interaction />
       <Card1 />
       <Card1 />
