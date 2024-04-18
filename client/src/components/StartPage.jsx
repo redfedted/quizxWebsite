af5c3bfa0a66b43a6ad117241c58be22babc8ba0
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-19 00:33:30 +0530

    Commit

--- client/src/components/StartPage.jsx
+++ client/src/components/StartPage.jsx
@@ -2,7 +2,7 @@ import img2 from "/src/images/startpage2.png";
 import img1 from "/src/images/startpage1.png";
 function StartPage({ setStartPage }) {
   return (
-    <div className="mx-[0.826rem]">
+    <div className="mx-[1.3rem]">
       <h1 className="mb-[5.494rem] mt-[1.791rem] text-center text-[3.401rem]  font-bold text-[#CF0804]">
         QuizX
       </h1>
@@ -11,17 +11,17 @@ function StartPage({ setStartPage }) {
         <img src={img2} className="mt-[13.063rem] shadow-2xl" alt="" />
         <img className="absolute bottom-0 right-0 shadow-2xl" src={img1} alt="" />
       </div>
-      <div className="mx-[0.666rem] mb-[0.5rem] text-[2.25rem] font-bold">
+      <div className=" mb-[0.5rem] text-[2.25rem] font-bold">
         {" "}
         <h2>Introducing</h2>
         <h2 className="text-[#D2322F]">Campus Ecosystem</h2>
       </div>
 
-      <h3 className="mx-[0.666rem] mb-[1.5rem] w-9/12 text-[1.5rem] leading-[28.77pxrem] opacity-75">
+      <h3 className="mb-[1.5rem] w-10/12 text-[1.5rem] leading-[28.77pxrem] opacity-75">
         Reimagine the Educational Experience for your Institution
       </h3>
       <button
-        className="border-1 mx-[0.666rem] mb-[2.563rem]  rounded-full border-2 border-black px-[2.5rem] py-[1rem] text-[1rem]"
+        className="border-1  mb-[2.563rem]  rounded-full border-2 border-black px-[2.5rem] py-[1rem] text-[1rem]"
         onClick={() => {
           setStartPage(false);
         }}
