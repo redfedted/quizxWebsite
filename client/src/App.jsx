679f66c6e3f93d7c2e870e47f5023c47b709eb4c
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-18 21:43:38 +0530

    Cpommit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import About from "./components/About";
 import Card1 from "./components/Card1";
 import Card2 from "./components/Card2";
@@ -7,27 +8,41 @@ import Info from "./components/Info";
 import Interaction from "./components/Interaction";
 import Loop from "./components/Loop";
 import Navbar from "./components/Navbar";
+import StartPage from "./components/StartPage";
 import "./index.css";
 
 function App() {
+  const [startPage, setStartPage] = useState(true);
   return (
     <div>
-      <Navbar />
-      <Info />
-      <Loop />
-      <Interaction />
-      <Card1 h1={"Group"} h2={"Connect and Collaborate Anytime, Anywhere."} />
-      <Card1 h1={"Switch"} h2={"Flip the Switch to Focus and Ideate"} />
-      <Card1 h1={"Corridor"} h2={"Discover and Connect"} />
-      <Card1 h1={"Trivia"} h2={"Learn, Play, Grow Every Day"} />
-      <Card2 />
-      <Card1 />
-      <Card1 />
-      <Card1 />
-      <Card2 />
-      <Card1 />
-      <Contact />
-      <Footer />
+      {startPage ? (
+        <div>
+          <StartPage setStartPage={setStartPage} />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <Info />
+          <Loop />
+          <Interaction />
+          <Card1
+            h1={"Group"}
+            h2={"Connect and Collaborate Anytime, Anywhere."}
+          />
+          <Card1 h1={"Switch"} h2={"Flip the Switch to Focus and Ideate"} />
+          <Card1 h1={"Corridor"} h2={"Discover and Connect"} />
+          <Card1 h1={"Trivia"} h2={"Learn, Play, Grow Every Day"} />
+          <Card2 />
+          <Card1 />
+          <Card1 />
+          <Card1 />
+          <Card2 />
+          <Card1 />
+          <Contact />
+          <Footer />
+        </div>
+      )}
+      `
     </div>
   );
 }
---
+++ client/src/components/StartPage.jsx
@@ -0,0 +1,35 @@
+import img2 from "/src/images/startpage2.png";
+import img1 from "/src/images/startpage1.png";
+function StartPage({ setStartPage }) {
+  return (
+    <div className="mx-[0.826rem]">
+      <h1 className="mb-[5.494rem] mt-[1.791rem] text-center text-[3.401rem]  font-bold text-[#CF0804]">
+        QuizX
+      </h1>
+      <div className="relative mb-[1.056rem]">
+        {" "}
+        <img src={img2} className="mt-[13.063rem] shadow-2xl" alt="" />
+        <img className="absolute bottom-0 right-0 shadow-2xl" src={img1} alt="" />
+      </div>
+      <div className="mx-[0.666rem] mb-[0.5rem] text-[2.25rem] font-bold">
+        {" "}
+        <h2>Introducing</h2>
+        <h2 className="text-[#D2322F]">Campus Ecosystem</h2>
+      </div>
+
+      <h3 className="mx-[0.666rem] mb-[1.5rem] w-9/12 text-[1.5rem] leading-[28.77pxrem] opacity-75">
+        Reimagine the Educational Experience for your Institution
+      </h3>
+      <button
+        className="border-1 mx-[0.666rem] mb-[2.563rem]  rounded-full border-2 border-black px-[2.5rem] py-[1rem] text-[1rem]"
+        onClick={() => {
+          setStartPage(false);
+        }}
+      >
+        Explore more
+      </button>
+    </div>
+  );
+}
+
+export default StartPage;
