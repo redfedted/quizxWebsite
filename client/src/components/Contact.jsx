93114b9f36530d496798d6cb4b6370a60bc41e79
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-18 23:01:49 +0530

    Commit

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import svg from "/src/svgs/Handshake.svg";
 function Contact() {
   return (
-    <div className="flex flex-col gap-[1rem] items-center text-center bg-[#CF0804] py-[1.235rem] px-[2.959rem] mb-[6.672rem]">
+    <div className="flex flex-col mt-[2.563rem] gap-[1rem] items-center text-center bg-[#CF0804] py-[1.235rem] px-[2.959rem] mb-[6.672rem]">
       <div className="flex gap-[0.25rem] bg-white rounded-full text-[0.684rem] px-[1.875rem] py-[0.734rem] border border-solid border-1 border-black">
         <img src={svg} alt="" />
         <button className="">Human assistance</button>
