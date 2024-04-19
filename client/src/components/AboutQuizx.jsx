dac5f0d3215b38fc5e8bb6cb521c6a0b2417a974
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-19 20:10:22 +0530

    Collapse

--- client/src/components/AboutQuizx.jsx
+++ client/src/components/AboutQuizx.jsx
@@ -11,11 +11,16 @@ function AboutQuizx({ heading, description, imgUrl }) {
       <div className="mx-[1.134rem] flex flex-col items-start ">
         <h1 className="w-9/12 pb-[1rem] text-[1.5rem] font-black">{heading}</h1>
         <h2 className="w-9/12 pb-[1rem]  text-[1rem]">{description}</h2>
-
-        <ol
-          className="list-disc pl-[1.134rem] "
-          {...getCollapseProps()}
-        >
+        {isExpanded ? null : (
+          <div
+            className="mb-[2.188rem]   flex gap-[0.25rem]  rounded-[0.5rem] bg-[#ECECEC] px-[1.5rem] py-[0.5rem]"
+            {...getToggleProps()}
+          >
+            <button className="text-[0.75rem]">View more</button>
+            <img src={svg} alt="" />
+          </div>
+        )}
+        <ol className="list-disc pl-[1.134rem] " {...getCollapseProps()}>
           <li>
             Interactive and Gamified Learning for engaging students with lively,
             memorable education tools.
@@ -37,13 +42,6 @@ function AboutQuizx({ heading, description, imgUrl }) {
             knowledge efficiently.
           </li>
         </ol>
-        <div
-          className="mb-[2.188rem]   flex gap-[0.25rem]  rounded-[0.5rem] bg-[#ECECEC] px-[1.5rem] py-[0.5rem]"
-          {...getToggleProps()}
-        >
-          <button className="text-[0.75rem]">View more</button>
-          <img src={svg} alt="" />
-        </div>
       </div>
       <div className="flex justify-center">
         {" "}
