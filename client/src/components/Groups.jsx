3f59d54ff4fe3b2e75a7e1c609719a244fa5bfb7
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-19 22:55:23 +0530

    Commit

--- client/src/components/Groups.jsx
+++ client/src/components/Groups.jsx
@@ -1,12 +1,60 @@
-import svg from "/src/svgs/arrowdown.svg";
+import { useEffect, useState } from "react";
+import svg1 from "/src/svgs/down.svg";
+import svg2 from "/src/svgs/up.svg";
+import { useCollapse } from "react-collapsed";
+
 function Groups({ title, description }) {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+
   return (
-    <div className="mx-[1rem] mb-[1rem] flex items-center justify-between  rounded-[0.5rem] border-[0.063rem] border-solid border-[#787878] px-[1.509rem] py-[1.05rem]">
+    <div
+      className={`mx-[1rem] mb-[1rem] flex items-start ${isExpanded && "bg-[#393939]"}  justify-between rounded-[0.5rem] border-[0.063rem] border-solid border-[#787878] px-[1.509rem] pt-[1.05rem]`}
+    >
       <div>
-        <h1 className="text-[1.25rem]">{title}</h1>
-        <h2 className="text-[0.75rem] text-[#696969]">{description}</h2>
+        <h1 className={`text-[1.25rem] ${isExpanded && "text-white"}`}>
+          {title}
+        </h1>
+        <h2
+          className={`pb-[1.063rem] text-[0.75rem] text-[#696969] ${isExpanded && "text-[#C5C5C5]"}`}
+        >
+          {description}
+        </h2>{" "}
+        <div {...getCollapseProps()}>
+          <h1
+            className={` pb-[1rem] text-[1.049rem] ${isExpanded && "text-white"}`}
+          >
+            Groups bring the classroom to you, wherever you are. It’s a
+            dedicated space for teachers and students to communicate, share
+            resources, and support each other’s learning journey. Simple,
+            effective, and always accessible, Groups ensures that education
+            doesn’t end at the classroom door—it’s where the extended learning
+            community thrives.
+          </h1>
+        </div>
       </div>
-      <img src={svg} alt="" />
+      <div {...getToggleProps()} className="pt-2 ">
+        {isExpanded ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="#ffffff"
+            viewBox="0 0 256 256"
+          >
+            <path d="M213.66,165.66a8,8,0,0,1-11.32,0L128,91.31,53.66,165.66a8,8,0,0,1-11.32-11.32l80-80a8,8,0,0,1,11.32,0l80,80A8,8,0,0,1,213.66,165.66Z"></path>
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="#383838"
+            viewBox="0 0 256 256"
+          >
+            <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
+          </svg>
+        )}
+      </div>
     </div>
   );
 }
