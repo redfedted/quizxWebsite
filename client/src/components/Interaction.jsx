a39a194b0e5662a6c05dff6625ee65e290d35c34
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-23 00:25:08 +0530

    Commit

--- client/src/components/Interaction.jsx
+++ client/src/components/Interaction.jsx
@@ -4,13 +4,13 @@ function Interaction() {
   return (
     <>
       <div className="px-[1rem]">
-        <div className="lg:mx-[4.5%] mb-[1.25rem] text-center text-[1.25rem] font-bold lg:text-start lg:text-[2.667rem] ">
+        <div className="mb-[1.25rem] text-center text-[1.25rem] font-bold lg:mx-[4.5%] lg:text-start lg:text-[2.667rem] ">
           {" "}
           <h1>Discover the QuizX Difference</h1>
           <h2 className="text-[#CF0804]">Transform Your Campus Today.</h2>
         </div>
       </div>
-      <div className="lg:flex lg:justify-between   lg:mx-[4.5%]">
+      <div className="lg:mx-[4.5%]  lg:grid lg:grid-cols-3  lg:justify-between">
         <div>
           <Groups
             title={"Groups"}
